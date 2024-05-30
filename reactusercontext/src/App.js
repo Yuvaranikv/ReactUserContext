b4cdb6afc6b0f87d0368906c2f75e40e62c43668
yuvaranik <yuvaranikv@gmail.com>
2024-05-30 18:45:18 +0530

    Added bootstrtap navbar

--- reactusercontext/src/App.js
+++ reactusercontext/src/App.js
@@ -3,14 +3,43 @@ import React, { useState } from 'react';
 import UserContext from './UserContext';
 
 import ComponentA from './ComponentA';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [user] = useState({ username: 'timk' });
 
   return (
     <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <UserContext.Provider value={user}>
-       <ComponentA />
+        <ComponentA />
       </UserContext.Provider>
     </div>
   );
